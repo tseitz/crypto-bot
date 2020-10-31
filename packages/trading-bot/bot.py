5e123c3e2af93b40b8c7a16954123f102fca95fd
Tegan Seitz <tseitz@pop-os.localdomain>
2020-10-30 19:02:30 -0500

    Add simple RSI reading

--- packages/trading-bot/bot.py
+++ packages/trading-bot/bot.py
@@ -1,7 +1,20 @@
-import websocket, json, pprint
+import websocket, json, pprint, numpy
+from talib import RSI
+
+# from binance.client import Client
+# from binanace.enums import *
 
 SOCKET = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
+RSI_PERIOD = 14
+RSI_OVERBOUGHT = 70
+RSI_OVERSOLD = 30
+TRADE_SYMBOL = 'ETHUSD'
+TRADE_QUANTITY = .05
+
+closes = []
+
+# client = Client(process.env.API_KEY, process.env.API_SECRET, tld='us')
 
 def on_open(ws):
     print("opened connection")
@@ -13,7 +26,7 @@ def on_close(ws):
 
 def on_message(ws, message):
     json_message = json.loads(message)
-    pprint.pprint(json_message)
+    # pprint.pprint(json_message)
 
     candle = json_message["k"]
 
@@ -23,6 +36,13 @@ def on_message(ws, message):
 
     if is_candle_closed:
         print(f"candle closed at {close}")
+        closes.append(float(close))
+        print(closes)
+
+        if len(closes) > RSI_PERIOD:
+            np_closes = numpy.array(closes)
+            rsi = RSI(np_closes, RSI_PERIOD)
+            print(rsi)
 
 
 ws = websocket.WebSocketApp(SOCKET, on_open=on_open, on_close=on_close, on_message=on_message)
