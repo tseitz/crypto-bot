915b9520db2129b5ece72d46f7b2cecd78be2c7c
Tegan Seitz <tseitz@pop-os.localdomain>
2020-11-02 09:29:05 -0600

    Parsing json from webhook endpoint

--- packages/trading-bot/server/index.js
+++ packages/trading-bot/server/index.js
@@ -1,13 +1,32 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+
 const app = express();
-const port = 3000;
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+app.use(jsonParser)
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/webhooks", (req, res) => {
-  res.send(req);
+app.post("/webhook/trading-view", jsonParser, (req, res) => {
+  if (req.body.passphrase !== process.env.TRADING_VIEW_PASSPHRASE) {
+    console.log("Hey buddy, get out of here");
+    res.sendStatus(401);
+    return;
+  }
+  res.send(req.body);
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Listening on port ${PORT}`);
+  if (PORT === 3000) {
+    console.log(`Here ya go http://localhost:${PORT}`);
+  } else {
+    console.log(`Hi Heroku`);
+  }
+});
