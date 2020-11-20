684d33357bc7a0172827564d8b9b595bd0c51e90
Tegan Seitz <tseitz@pop-os.localdomain>
2020-11-20 15:36:58 -0600

    We has transaction!

--- packages/trading-bot-server/services/uniswapService.ts
+++ packages/trading-bot-server/services/uniswapService.ts
@@ -10,13 +10,19 @@ import {
   TokenAmount,
   TradeType,
 } from '@uniswap/sdk';
-import { ethers } from 'ethers';
+import { ethers, utils, BigNumber } from 'ethers';
+import axios from 'axios';
+import { toWei } from 'web3-utils';
 import abis from '../abis/abis';
 
 const chainId = ChainId.ROPSTEN;
 // const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F'; // dai
 // const DAI = new Token(ChainId.MAINNET, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18);
-const DAI = new Token(ChainId.ROPSTEN, '0xad6d458402f60fd3bd25163575031acdce07538d', 18);
+const DAI = new Token(
+  ChainId.ROPSTEN,
+  utils.getAddress('0xad6d458402f60fd3bd25163575031acdce07538d'),
+  18
+);
 
 // Use the mainnet
 const network = 'ropsten';
@@ -55,8 +61,10 @@ export async function getPair(): Promise<Pair> {
   const pair = await Fetcher.fetchPairData(DAI, WETHID);
 
   const route = new Route([pair], WETHID);
+  const tradeValue = 50 / parseInt(route.midPrice.toSignificant(6)); // risk $50
   console.log(route.midPrice.toSignificant(6));
-  console.log(route.midPrice.invert().toSignificant(6));
+  // console.log(route.midPrice.invert().toSignificant(6)); // amount in dai
+  console.log(tradeValue);
 
   const trade = new Trade(
     route,
@@ -64,16 +72,20 @@ export async function getPair(): Promise<Pair> {
     TradeType.EXACT_INPUT
   );
 
-  console.log(trade.executionPrice.toSignificant(6));
-  console.log(trade.nextMidPrice.toSignificant(6)); // we can see what price we'll get after the trade
+  // console.log(trade.executionPrice.toSignificant(6));
+  // console.log(trade.nextMidPrice.toSignificant(6)); // we can see what price we'll get after the trade
 
   const slippageTolerance = new Percent('50', '10000'); // 50 bips, or 0.50%
 
   const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw; // worst case scenario
   const path = [WETHID.address, DAI.address];
-  const to = '0x266d6Bc2262Cc2690Ef5C0313e7330995C15eEDb'; // my address
+  const to = utils.getAddress('0x266d6Bc2262Cc2690Ef5C0313e7330995C15eEDb'); // my address
   const deadline = Math.floor(Date.now() / 1000) + 60 * 20; // 20 minutes from the current Unix time
-  const value = trade.inputAmount.raw; // needs to be converted to e.g. hex
+  // const value = trade.inputAmount.raw; // needs to be converted to e.g. hex
+  const value = BigNumber.from(toWei('1', 'ether'));
+  // const value = utils.hexlify(50000000000000000); // 0.05ETH
+  const gasPrice = await getGasPrice();
+  console.log(gasPrice);
   console.log(value);
 
   // call a contract with order details
@@ -83,9 +95,9 @@ export async function getPair(): Promise<Pair> {
   //   { "internalType": "address", "name": "to", "type": "address" },
   //   { "internalType": "uint256", "name": "deadline", "type": "uint256" }
   // ],
-  const tx = await uniswap.swapExactETHForTokens(amountOutMin, path, to, deadline, {
+  const tx = await uniswap.swapExactETHForTokens(value, path, to, deadline, {
     value,
-    gasPrice: 20e9,
+    gasPrice,
   });
   console.log(`Transaction Hash: ${tx.hash}`);
 
@@ -98,3 +110,15 @@ export async function getPair(): Promise<Pair> {
 // async function getDecimals(chainId: ChainId, tokenAddress: string): Promise<number> {
 //   // implementation details
 // }
+
+export async function getGasPrice(speed = 'average') {
+  // , maxWait?: number = 100) {
+  try {
+    const response = await axios.get('https://ethgasstation.info/json/ethgasAPI.json');
+    const price = response.data[speed];
+    // const price = Object.keys(data).find((price) => parseFloat(data[price]) <= maxWait);
+    return toWei(`${(price || response.data.fast) / 10}`, 'gwei');
+  } catch (error) {
+    throw new Error(`Failed to fetch gas price data: ${error}`);
+  }
+}
