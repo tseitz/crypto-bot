46b487195fd60f899bff749b3a3254189b40e4d9
Tegan Seitz <tseitz@pop-os.localdomain>
2020-11-29 21:11:31 -0600

    Lower non leverage positions

--- models/kraken/KrakenOrderDetails.ts
+++ models/kraken/KrakenOrderDetails.ts
@@ -105,7 +105,7 @@ export default class KrakenOrderDetails {
     } else {
       // let's risk $200 for now
       volume = Number.parseFloat(
-        ((75 * (this.leverageAmount || 1.5)) / this.usdValueOfBase).toFixed(this.volumeDecimals)
+        ((75 * (this.leverageAmount || 1.1)) / this.usdValueOfBase).toFixed(this.volumeDecimals)
       );
     }
     return volume > this.minVolume ? volume : this.minVolume;
