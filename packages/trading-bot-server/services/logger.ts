628fbdc38023cd0769ae22283b99a774a4a02482
Tegan Seitz <tseitz@pop-os.localdomain>
2020-12-02 09:07:35 -0600

    New logger. Cancel open opposite orders first

--- packages/trading-bot-server/services/logger.ts
+++ packages/trading-bot-server/services/logger.ts
@@ -1,1 +1,9 @@
-export function logResult() {}
+import { KrakenOrderResponse } from '../models/kraken/KrakenResults';
+
+export function logOrderResult(comment: string, response: KrakenOrderResponse) {
+  if (response.error.length > 0) {
+    console.log(`${comment}: ERROR ${response.error}`);
+  } else {
+    console.log(`${comment}: ${response.result?.descr.order}`);
+  }
+}
