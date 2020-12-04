4fc444dfd9821b022da12ce9fc4867c3f106effa
tseitz <tdseitz10@gmail.com>
2020-12-03 21:31:29 -0600

    Lower margin level

--- services/krakenService.ts
+++ services/krakenService.ts
@@ -160,7 +160,7 @@ class KrakenService {
         }
       }
 
-      if (add && positionMargin < 300) {
+      if (add && positionMargin < 250) {
         result = await this.kraken.setAddOrder({
           pair: order.krakenTicker,
           type: order.action,
@@ -215,7 +215,7 @@ class KrakenService {
         });
       }
     } else {
-      if (order.balanceOfBase < 350) {
+      if (order.balanceOfBase < 275) {
         if (order.balanceOfBase < 1e-5) {
           console.log('New Entry');
           result = await this.kraken.setAddOrder({
