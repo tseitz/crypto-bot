896caa355f8b721e12f02dee7de14612a6e101d6
tseitz <tdseitz10@gmail.com>
2021-01-11 08:00:37 -0600

    Update buy bags

--- packages/trading-bot-server/services/krakenService.ts
+++ packages/trading-bot-server/services/krakenService.ts
@@ -294,10 +294,7 @@ class KrakenService {
     if (order.buyBags) {
       // buy 40% worth of my usd available
       // currently morphing original order. Sorry immutability
-      tradeVolumeInDollar = order.superParseFloat(
-        order.balanceOfQuote * 0.25,
-        order.volumeDecimals
-      );
+      tradeVolumeInDollar = order.superParseFloat(order.balanceOfQuote * 0.4, order.volumeDecimals);
     } else {
       // sell 80% worth of currency available
       tradeVolumeInDollar = order.superParseFloat(
