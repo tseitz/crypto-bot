6af838a241fb581998b1d7e19a22c3590c3e3f43
tseitz <tdseitz10@gmail.com>
2021-01-25 18:07:18 -0600

    Sell a smaller amount of non leverage first

--- packages/trading-bot-server/services/krakenService.ts
+++ packages/trading-bot-server/services/krakenService.ts
@@ -293,7 +293,7 @@ class KrakenService {
             type: newOrder.action,
             ordertype: 'limit',
             price: newOrder.bidPrice,
-            volume: incrementalAddVolume,
+            volume: order.tradeVolume,
             // validate: order.validate,
           });
           logOrderResult(`Sell Non Leveraged Order`, result, order.krakenizedTradingViewTicker);
