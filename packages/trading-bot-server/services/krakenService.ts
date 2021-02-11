c1fed6df0e7f378b176e73df282fb9bfd0c5ef46
tseitz <tdseitz10@gmail.com>
2021-02-11 11:03:09 -0600

    Bump boost for non leverage

--- packages/trading-bot-server/services/krakenService.ts
+++ packages/trading-bot-server/services/krakenService.ts
@@ -221,7 +221,7 @@ class KrakenService {
             100
           ).toFixed(2)
         );
-        const boostPercentDiff = percentDiff * -1;
+        const boostPercentDiff = percentDiff * -2;
         const boost = parseFloat((1 + boostPercentDiff / 100).toFixed(2));
         const incrementalAddVolume = (order.addVolume * boost).toFixed(order.volumeDecimals);
         const incrementalAddDollar = ((order.positionSize || order.addSize) * boost).toFixed(2);
