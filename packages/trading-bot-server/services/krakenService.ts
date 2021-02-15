601b9b784d22d4428d7db0a74b437360da866585
tseitz <tdseitz10@gmail.com>
2021-02-15 13:54:51 -0600

    Bump lowest margin level

--- packages/trading-bot-server/services/krakenService.ts
+++ packages/trading-bot-server/services/krakenService.ts
@@ -151,7 +151,7 @@ class KrakenService {
         if (
           percentDiff < 1 &&
           percentDiff > -1 &&
-          marginPositionAfter > 780 &&
+          marginPositionAfter > 850 &&
           order.marginFree < order.lowestLeverageMargin * 2
         ) {
           console.log('Position within 1%. Margin too low. Ignoring.');
