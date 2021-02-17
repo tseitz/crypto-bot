54f5aaf84ec0a4421204bd09fb600a2ff12352eb
tseitz <tdseitz10@gmail.com>
2021-02-17 07:43:13 -0600

    Bump delev

--- packages/trading-bot-server/models/kraken/KrakenOrderDetails.ts
+++ packages/trading-bot-server/models/kraken/KrakenOrderDetails.ts
@@ -123,7 +123,7 @@ export default class KrakenOrderDetails {
     this.strategyParams = strategyParams[this.tradingViewTicker];
     this.originalEntry = this.strategyParams?.entrySize;
     this.originalAdd = this.strategyParams?.addSize;
-    this.shortZoneDeleverage = 0.7;
+    this.shortZoneDeleverage = 0.8;
     this.entrySize = !this.shortZone
       ? this.strategyParams?.entrySize
       : this.strategyParams?.entrySize * this.shortZoneDeleverage;
