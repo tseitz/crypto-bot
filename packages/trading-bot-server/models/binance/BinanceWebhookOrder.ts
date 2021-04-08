81df32dad7f98721eeba8fa099c0484369e7b8cf
tseitz <tdseitz10@gmail.com>
2021-04-08 13:28:08 -0500

    Begin binance integration

---
+++ packages/trading-bot-server/models/binance/BinanceWebhookOrder.ts
@@ -0,0 +1,103 @@
+import { KrakenOrderResponse } from "./KrakenResults";
+import { TradingViewBody } from "../TradingViewBody";
+import { kraken } from "../../services/krakenService";
+import KrakenOrderDetails from "./KrakenOrderDetails";
+
+export class BinanceWebhookOrder {
+  requestBody: TradingViewBody;
+  tradingViewTicker: string;
+  binanceTicker: string;
+  order?: BinanceOrderDetails;
+
+  constructor(requestBody: TradingViewBody) {
+    this.requestBody = requestBody;
+    this.tradingViewTicker = requestBody.ticker;
+  }
+
+  async placeOrder() {
+    try {
+      // set up the order
+      this.order = new KrakenOrderDetails(await this.initOrder());
+
+      // execute the order
+      return await this.openOrder(this.order);
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+
+  private async initOrder() {
+    // get pair data
+    const pairData = await kraken.getPair(this.krakenTicker);
+
+    // get pair price info for order
+    const pairPriceInfo = await kraken.getPrice(this.krakenTicker);
+
+    // btc or eth price for calculations (we're currently placing orders in fixed USD amount)
+    const assetClass = this.krakenTicker.includes("XBT")
+      ? "XBTUSDT"
+      : "ETHUSDT";
+    const assetClassPriceInfo = await kraken.getPrice(assetClass);
+
+    const myBalanceInfo = await kraken.getBalance();
+
+    const openOrders = await kraken.getOpenOrders();
+
+    const tradeBalance = await kraken.getTradeBalance();
+
+    return {
+      body: this.requestBody,
+      krakenizedTicker: this.krakenTicker,
+      pairData,
+      pairPriceInfo,
+      assetClassPriceInfo,
+      myBalanceInfo,
+      openOrders,
+      tradeBalance,
+    };
+  }
+
+  private async openOrder(
+    order: KrakenOrderDetails
+  ): Promise<KrakenOrderResponse | undefined> {
+    console.log(`Margin Free: ${order.marginFree}`);
+    console.log(`Price: ${order.tradingViewPrice} | Bid: ${order.bidPrice}`);
+
+    let result;
+    if (order.oldest) {
+      result = await kraken.sellOldestOrders(order, order.krakenTicker);
+    } else if (order.bagIt) {
+      if (!order.nonLeverageOnly) {
+        // close order first, handle bags so funds are available, then handle leverage
+        result = await kraken.settleLeveragedOrder(order);
+
+        // wait a second for settle to go through
+        setTimeout(async () => {
+          result = await kraken.handleLeveragedOrder(order);
+        }, 5000);
+
+        // just wait this one out for now
+        setTimeout(async () => {
+          // Reset order details based on above order. Mostly margin free...
+          // TODO: this needs improved
+          const newOrderInfo = await this.initOrder();
+          const order = new KrakenOrderDetails(newOrderInfo);
+          result = await kraken.handleBags(order);
+        }, 105000);
+      } else {
+        result = await kraken.handleBags(order);
+      }
+    } else if (order.noLeverage) {
+      result = await kraken.handleNonLeveragedOrder(order);
+    } else {
+      if (order.close) {
+        result = await kraken.settleLeveragedOrder(order);
+      } else {
+        result = await kraken.handleLeveragedOrder(order);
+      }
+    }
+
+    return result;
+  }
+}
