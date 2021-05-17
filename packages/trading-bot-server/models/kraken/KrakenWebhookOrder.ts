c99af1ea5e5341f844cd614b6bfd637dd7a867e8
tseitz <tdseitz10@gmail.com>
2021-05-17 12:29:18 -0500

    Add short zone logging

--- packages/trading-bot-server/models/kraken/KrakenWebhookOrder.ts
+++ packages/trading-bot-server/models/kraken/KrakenWebhookOrder.ts
@@ -1,58 +1,56 @@
-import { KrakenOrderResponse } from "./KrakenResults";
-import { KrakenTradingViewBody } from "../TradingViewBody";
-import { kraken } from "../../services/krakenService";
-import KrakenOrderDetails from "./KrakenOrderDetails";
+import { KrakenOrderResponse } from './KrakenResults'
+import { KrakenTradingViewBody } from '../TradingViewBody'
+import { kraken } from '../../services/krakenService'
+import KrakenOrderDetails from './KrakenOrderDetails'
 
 export class KrakenWebhookOrder {
-  requestBody: KrakenTradingViewBody;
-  tradingViewTicker: string;
-  krakenTicker: string;
-  order?: KrakenOrderDetails;
+  requestBody: KrakenTradingViewBody
+  tradingViewTicker: string
+  krakenTicker: string
+  order?: KrakenOrderDetails
 
   constructor(requestBody: KrakenTradingViewBody) {
-    this.requestBody = requestBody;
-    this.tradingViewTicker = requestBody.ticker;
+    this.requestBody = requestBody
+    this.tradingViewTicker = requestBody.ticker
     // Kraken uses XBT instead of BTC. Uniswap uses WETH instead of ETH
     // I use binance/uniswap for most webhooks since there is more volume
     this.krakenTicker = this.tradingViewTicker
-      .replace("BTC", "XBT")
-      .replace("WETH", "ETH")
-      .replace("DOGE", "XDG")
-      .replace("USDT", "USD");
+      .replace('BTC', 'XBT')
+      .replace('WETH', 'ETH')
+      .replace('DOGE', 'XDG')
+      .replace('USDT', 'USD')
   }
 
   async placeOrder() {
     try {
       // set up the order
-      const orderDetails = await this.initOrder();
-      this.order = new KrakenOrderDetails(orderDetails);
+      const orderDetails = await this.initOrder()
+      this.order = new KrakenOrderDetails(orderDetails)
 
       // execute the order
-      return await this.openOrder(this.order);
+      return await this.openOrder(this.order)
     } catch (error) {
-      console.log(error);
-      return error;
+      console.log(error)
+      return error
     }
   }
 
   private async initOrder() {
     // get pair data
-    const pairData = await kraken.getPair(this.krakenTicker);
+    const pairData = await kraken.getPair(this.krakenTicker)
 
     // get pair price info for order
-    const pairPriceInfo = await kraken.getPrice(this.krakenTicker);
+    const pairPriceInfo = await kraken.getPrice(this.krakenTicker)
 
     // btc or eth price for calculations (we're currently placing orders in fixed USD amount)
-    const assetClass = this.krakenTicker.includes("XBT")
-      ? "XBTUSDT"
-      : "ETHUSDT";
-    const assetClassPriceInfo = await kraken.getPrice(assetClass);
+    const assetClass = this.krakenTicker.includes('XBT') ? 'XBTUSDT' : 'ETHUSDT'
+    const assetClassPriceInfo = await kraken.getPrice(assetClass)
 
-    const myBalanceInfo = await kraken.getBalance();
+    const myBalanceInfo = await kraken.getBalance()
 
-    const openOrders = await kraken.getOpenOrders();
+    const openOrders = await kraken.getOpenOrders()
 
-    const tradeBalance = await kraken.getTradeBalance();
+    const tradeBalance = await kraken.getTradeBalance()
 
     return {
       body: this.requestBody,
@@ -63,27 +61,29 @@ export class KrakenWebhookOrder {
       myBalanceInfo,
       openOrders,
       tradeBalance,
-    };
+    }
   }
 
   private async openOrder(
     order: KrakenOrderDetails
   ): Promise<KrakenOrderResponse | undefined> {
-    console.log(`Margin Free: ${order.marginFree}`);
-    console.log(`Price: ${order.tradingViewPrice} | Bid: ${order.bidPrice}`);
+    console.log(`Margin Free: ${order.marginFree}`)
+    console.log(
+      `Price: ${order.tradingViewPrice} | Bid: ${order.bidPrice} | ${order.shortZone}`
+    )
 
-    let result;
+    let result
     if (order.oldest) {
-      result = await kraken.sellOldestOrders(order, order.krakenTicker);
+      result = await kraken.sellOldestOrders(order, order.krakenTicker)
     } else if (order.bagIt) {
       if (!order.nonLeverageOnly) {
         // close order first, handle bags so funds are available, then handle leverage
-        result = await kraken.settleLeveragedOrder(order);
+        result = await kraken.settleLeveragedOrder(order)
 
         // wait a second for settle to go through
         setTimeout(async () => {
-          result = await kraken.handleLeveragedOrder(order);
-        }, 5000);
+          result = await kraken.handleLeveragedOrder(order)
+        }, 5000)
 
         // // just wait this one out for now
         // setTimeout(async () => {
@@ -94,18 +94,18 @@ export class KrakenWebhookOrder {
         //   result = await kraken.handleBags(order);
         // }, 105000);
       } else {
-        result = await kraken.handleBags(order);
+        result = await kraken.handleBags(order)
       }
     } else if (order.noLeverage) {
-      result = await kraken.handleNonLeveragedOrder(order);
+      result = await kraken.handleNonLeveragedOrder(order)
     } else {
       if (order.close) {
-        result = await kraken.settleLeveragedOrder(order);
+        result = await kraken.settleLeveragedOrder(order)
       } else {
-        result = await kraken.handleLeveragedOrder(order);
+        result = await kraken.handleLeveragedOrder(order)
       }
     }
 
-    return result;
+    return result
   }
 }
